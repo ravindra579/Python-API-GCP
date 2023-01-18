34142b193e92eb092d83f5a9c1d347c8f4526bd2
SIDDAM RAVINDRA REDDY <64942306+ravindra579@users.noreply.github.com>
2023-01-18 10:13:04 +0530

    Update restapi.py

--- restapi.py
+++ restapi.py
@@ -33,8 +33,8 @@ docs = FlaskApiSpec(app)
 app.config.update({
     'HOST_INFO':"redis-19210.c1.asia-northeast1-1.gce.cloud.redislabs.com",
     "REDIS_PORT":19210,
-    'REDIS_PASSWORD':"44qFEeWsnSS9nOdxitHtFRbnaZCsWd24",
-    "PRIVATE_KEY":"Ravindra@"
+    'REDIS_PASSWORD':"",
+    "PRIVATE_KEY":""
 })
 
 
